2469a51620cf32a8ed89c99ec9e833ebda89ef87
Alina Voronina <alina.voronina@ucu.edu.ua>
2022-05-29 02:03:55 +0300

    added algorithm for several images stitching

--- stitching.py
+++ stitching.py
@@ -1,16 +1,16 @@
 import cv2
+from cv2 import ROTATE_90_CLOCKWISE
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 from knn import KnnClassifier
 from ransac import *
 
-
 class Image_Stitching():
     """
     Image Stitching classifier.
     """
-    
+
     def __init__(self, crossCheck=True):
         """
         Initialization of parameters.
@@ -22,11 +22,13 @@ class Image_Stitching():
         self.good_points = []
         self.doCrossCheck = crossCheck #can be changed
 
+
     def crossCheck(self, first_matches, second_matches):
         """
         Function to perform Cross-Check validation to exclude possibly
         incorrect matches. Returns matches1to2 - a list of matches from image1
-        to image 2, and good_points, which is a list of indexes of matching keypoints.
+        to image 2, and good_points, which is a list of indexes of matching
+        keypoints.
         """
         for key, value in first_matches.items():
             #if no keypoint1 for any keypoint2 to match we ignore it.
@@ -39,6 +41,7 @@ class Image_Stitching():
             #otherwise ignore the keypoint too.
             else:
                 continue
+
 
     def feature_matching(self, img1, img2):
         """
@@ -67,7 +70,8 @@ class Image_Stitching():
                 self.good_points.append((key, value))
         
         # draw matches on picture.
-        img3 = cv2.drawMatches(img1, kp1, img2, kp2, self.matches1to2, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        img3 = cv2.drawMatches(img1, kp1, img2, kp2, self.matches1to2, None, \
+            flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
         cv2.imwrite('matching.jpg', img3)
 
         if len(self.good_points) > self.min_match:
@@ -75,42 +79,35 @@ class Image_Stitching():
             image2_kp = np.float32([kp2[pair[1]].pt for pair in self.good_points])
 
             # apply ransac and get the best homography matrix.
-            H = ransac(image2_kp, image1_kp, 1000, 5.0, 0.8) #5.0 a threshold value (can be changed)
+            H = ransac(image2_kp, image1_kp, 1000, 5.0, 0.8)
+            # 5.0 a threshold value (can be changed)
         else:
             raise(ValueError, 'Too few keypoints!')
 
         return H
 
-    # def create_mask(self, img1, img2, version):
-    #     height_img1, width_img1, width_img2 = img1.shape[0], img1.shape[1], img2.shape[1]
-    #     height_panorama, width_panorama = height_img1, width_img1 + width_img2
 
-    #     offset = int(self.smoothing_window_size / 2)
-    #     barrier = img1.shape[1] - int(self.smoothing_window_size / 2)
-    #     mask = np.zeros((height_panorama, width_panorama))
-
-    #     if version== 'left_image':
-    #         mask[:, barrier - offset:barrier + offset ] = np.tile(np.linspace(1, 0, 2 * offset ).T, (height_panorama, 1))
-    #         mask[:, :barrier - offset] = 1
-    #     else:
-    #         mask[:, barrier - offset :barrier + offset ] = np.tile(np.linspace(0, 1, 2 * offset ).T, (height_panorama, 1))
-    #         mask[:, barrier + offset:] = 1
-    
-    #     return cv2.merge([mask, mask, mask])
-
-    def blending(self, img1, img2):
-        H = self.feature_matching(img1,img2)
+    def stitch(self, img1, img2):
+        H = self.feature_matching(img1, img2)
         width = img1.shape[1] + img2.shape[1]
         height = max(img1.shape[0], img2.shape[0])
         result = cv2.warpPerspective(img2, H,  (width, height))
         result[0:img1.shape[0], 0:img1.shape[1]] = img1
-        return result
+
+        rows, cols = np.where(result[:, :, 0] != 0)
+        min_row, max_row = min(rows), max(rows) + 1
+        min_col, max_col = min(cols), max(cols) + 1
+        final_result = result[min_row:max_row, min_col:max_col, :]
+
+        return final_result
+
 
 def read_input():
     """
     Read the input paths to the images.
     """
-    print('Enter path to images you want to stitch, one by one, divided by enter.')
+    print('Enter path to images you want to stitch, one by one,\
+ divided by enter.')
     images = []
 
     while True:
@@ -123,24 +120,37 @@ def read_input():
     return images
 
 
-def main(argv1, argv2, index):
+def main(argv1, argv2, index, rotate=False):
     """
     Main function to start stitching images.
     """
     img1 = cv2.imread(argv1)
     img2 = cv2.imread(argv2)
-    final=Image_Stitching().blending(img1, img2)
+
+    if rotate:
+        img1 = cv2.rotate(img1, ROTATE_90_CLOCKWISE)
+        img2 = cv2.rotate(img2, ROTATE_90_CLOCKWISE)
+
+    final = Image_Stitching().stitch(img1, img2)
+
+    if rotate:
+        final = cv2.rotate(final, cv2.ROTATE_90_COUNTERCLOCKWISE)
     cv2.imwrite(f'picture{index}.jpg', final)
 
     return f'picture{index}.jpg'
 
 
 if __name__ == '__main__':
+    rotation = input('Input H if you want to stitch images horizintally,\
+ V if vertically: ')
+    rotate = True if rotation == 'V' else False
+
     images = read_input()
     counter = 0
-
-    for ind, image in enumerate(images[:-1]):
-        new_path = main(images[ind], images[ind+1], counter)
+    rev_images = images[::-1]
+    
+    for ind, image in enumerate(rev_images[:-1]):
+        new_path = main(images[ind], images[ind+1], counter, rotate)
         images[ind+1] = new_path
         counter += 1
     
